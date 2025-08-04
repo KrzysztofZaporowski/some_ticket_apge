fba2b6e17cf36b1c2653d4cb99151faa0042d2af
Krzysztof Zaporowski <krzysztofzaporowski@gmail.com>
2025-08-04 11:45:40 +0200

    Ticket generation

--- script.js
+++ script.js
@@ -1,8 +1,41 @@
-const userData = document.querySelector('.userData');
-const metaData = JSON.parse(localStorage.getItem('userData'))
+const metaData = JSON.parse(localStorage.getItem('userData'));
+const welcomeSection = document.querySelector('.welcome');
+const emailSection = document.querySelector('.email-info');
+const userImage = document.querySelector('.user-img');
+const userName = document.querySelector('.name');
+const userGithub = document.querySelector('.github-name');
+const right = document.querySelector('.right');
+const number = Math.floor(Math.random() * 100000);
 
-// userData.textContent = `${metaData.name}, ${metaData.email}, ${metaData.github}`;
+if (metaData && welcomeSection && emailSection && userImage && userName && userGithub && right) {
+  welcomeSection.innerHTML = `
+    <h1>Congrats, <span id="name">${metaData.name}</span>!</h1>
+    <h1>Your ticket is ready.</h1>
+  `;
 
-// Congrats, <!-- Full Name -->! Your ticket is ready.
+  emailSection.innerHTML = `
+    We've emailed your ticket to <span id="mail">${metaData.email}</span> and will send updates in the run up to the event.
+  `;
 
-//     We've emailed your ticket to <!-- Email Address --> and will send updates in the run up to the event.
+  userImage.innerHTML = `
+    <img src="${metaData.image}" alt="user-img">
+  `;
+
+  userName.innerHTML = `
+    ${metaData.name}
+  `;
+
+  userGithub.innerHTML = `
+    <img src="./assets/images/icon-github.svg" alt="github-icon">
+    ${metaData.github}
+  `;
+
+  right.innerHTML = `
+    <span id="number">#${number.toString().padStart(5, '0')}</span>
+  `;
+
+} else {
+  console.warn("Brakuje danych lub któregoś z elementów HTML.");
+}
+
+
